7f3582ac29c1c9d154677a2ef307f1f6de75fa84
Dominic Tobias <dominic.tobias@gmail.com>
2016-02-24 13:50:59 +0000

    Initial Commit

---
+++ MenuCalendar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenuCalendar
+//
+//  Created by dominic tobias on 24/02/2016.
+//  Copyright © 2016 Sekai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
